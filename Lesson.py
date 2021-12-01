ed682dd672c6e7917f937a7313121cd38f3fc02e
SashaAch <61113688+SashaAch@users.noreply.github.com>
2021-12-01 19:02:09 +0500

    Update Lesson.py
    Переписано хранение в списках, для каждого класса выделяется отдельный вложенный список. Добавлена функция подсчёта всех элементов вложенных списков в списках

--- Lesson.py
+++ Lesson.py
@@ -6,25 +6,28 @@ def img_list(path):
     return (os.path.join(path, f) for f in os.listdir(path))
 
 
-image_paths = []
-image_classes = []
-D = []
+# Функция подсчёта всех элементов в списке, включая вложенные списки
+def get_all_elements_in_list(lst):
+    count = 0
+    for element in lst:
+        count += len(element)
+    return count
+
 
 train_path = "dataset"  # каталог  с классами
-
 class_names = os.listdir(train_path)  # Имена классов по названию папок
 
-for training_name in class_names:  # Путь до каждой отдельной картинки в каждом классе
-    dir_ = os.path.join(train_path, training_name)
+D, image_paths = [[] for _ in range(len(class_names))], [[] for _ in range(len(class_names))]
+
+for i in range(len(class_names)):  # Путь до каждой отдельной картинки в каждом классе
+    dir_ = os.path.join(train_path, class_names[i])
     class_path = img_list(dir_)
-    image_paths += class_path
+    image_paths[i].extend(class_path)
 
 # Присваивание каждой картинке своего класса
-for training_name in class_names:
-    image_classes.extend([f'{training_name}'] * (len(image_paths) // 3))
-
-for i in range(len(image_paths)):  # Формирование списка D из пары путь + класс
-    D.append((image_paths[i], image_classes[i]))
+for i in range(len(D)):
+    for j in range(len(image_paths[0])):
+        D[i].append((image_paths[i][j], class_names[i]))
 
 # Разбиение списка на тренировочную и тестовую выборки
 dataset = []
@@ -32,19 +35,9 @@ dataset = []
 train = dataset[:36]
 test = dataset[36:]
 
-image_paths, y_train = zip(*train)
-image_paths_test, y_test = zip(*test)
+# image_paths, y_train = zip(*train)
+# image_paths_test, y_test = zip(*test)
 
-print(image_classes)
-print(len(image_classes))
-
-# print()
-print(len(D))
-# print(train)
-# print(test)
-#
-# print()
-# print(len(D))
-# print(class_names)
-# print(image_classes)
-# print(len(image_paths))
+print(image_paths)
+print()
+print(D)
