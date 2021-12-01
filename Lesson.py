c049a43bb2bc19710b5a2c3023e6e2a04d61111a
SashaAch <94893908+liz558@users.noreply.github.com>
2021-12-01 17:33:28 +0500

    Оптимизация кода

--- Lesson.py
+++ Lesson.py
@@ -20,17 +20,14 @@ for training_name in class_names:  # Путь до каждой отдельно
     image_paths += class_path
 
 # Присваивание каждой картинке своего класса
-image_classes_0 = ['Deer'] * (len(image_paths) // 3)
-image_classes_1 = ['Penguin'] * (len(image_paths) // 3)
-image_classes_2 = ['Pica'] * (len(image_paths) // 3)
-
-image_classes = image_classes_0 + image_classes_1 + image_classes_2
+for training_name in class_names:
+    image_classes.extend([f'{training_name}'] * (len(image_paths) // 3))
 
 for i in range(len(image_paths)):  # Формирование списка D из пары путь + класс
     D.append((image_paths[i], image_classes[i]))
 
 # Разбиение списка на тренировочную и тестовую выборки
-dataset = D
+dataset = []
 
 train = dataset[:36]
 test = dataset[36:]
@@ -38,12 +35,16 @@ test = dataset[36:]
 image_paths, y_train = zip(*train)
 image_paths_test, y_test = zip(*test)
 
-print(D)
-print(train)
-print(test)
-
-print()
-print(len(D))
-print(class_names)
 print(image_classes)
-print(len(image_paths))
+print(len(image_classes))
+
+# print()
+print(len(D))
+# print(train)
+# print(test)
+#
+# print()
+# print(len(D))
+# print(class_names)
+# print(image_classes)
+# print(len(image_paths))
