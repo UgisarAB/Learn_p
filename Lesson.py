f3b45673a8b81241bcde4acd5ef4f8419192e10b
SashaAch <achints@yandex.ru>
2021-12-01 18:57:09 +0500

    Revert "Оптимизация кода"
    This reverts commit 8b7515d071ffced71264de0211fa2ea4998a475c.

--- Lesson.py
+++ Lesson.py
@@ -6,28 +6,25 @@ def img_list(path):
     return (os.path.join(path, f) for f in os.listdir(path))
 
 
-# Функция подсчёта всех элементов в списке, включая вложенные списки
-def get_all_elements_in_list(lst):
-    count = 0
-    for element in lst:
-        count += len(element)
-    return count
-
+image_paths = []
+image_classes = []
+D = []
 
 train_path = "dataset"  # каталог  с классами
+
 class_names = os.listdir(train_path)  # Имена классов по названию папок
 
-D, image_paths = [[] for _ in range(len(class_names))], [[] for _ in range(len(class_names))]
-
-for i in range(len(class_names)):  # Путь до каждой отдельной картинки в каждом классе
-    dir_ = os.path.join(train_path, class_names[i])
+for training_name in class_names:  # Путь до каждой отдельной картинки в каждом классе
+    dir_ = os.path.join(train_path, training_name)
     class_path = img_list(dir_)
-    image_paths[i].extend(class_path)
+    image_paths += class_path
 
 # Присваивание каждой картинке своего класса
-for i in range(len(D)):
-    for j in range(len(image_paths[0])):
-        D[i].append((image_paths[i][j], class_names[i]))
+for training_name in class_names:
+    image_classes.extend([f'{training_name}'] * (len(image_paths) // 3))
+
+for i in range(len(image_paths)):  # Формирование списка D из пары путь + класс
+    D.append((image_paths[i], image_classes[i]))
 
 # Разбиение списка на тренировочную и тестовую выборки
 dataset = []
@@ -35,9 +32,19 @@ dataset = []
 train = dataset[:36]
 test = dataset[36:]
 
-# image_paths, y_train = zip(*train)
-# image_paths_test, y_test = zip(*test)
+image_paths, y_train = zip(*train)
+image_paths_test, y_test = zip(*test)
 
-print(image_paths)
-print()
-print(D)
+print(image_classes)
+print(len(image_classes))
+
+# print()
+print(len(D))
+# print(train)
+# print(test)
+#
+# print()
+# print(len(D))
+# print(class_names)
+# print(image_classes)
+# print(len(image_paths))
